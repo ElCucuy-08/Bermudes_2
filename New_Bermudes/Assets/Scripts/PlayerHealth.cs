0d78d6b4520ab5356dd29101d9f4ec40fc44f4ae
BogdanLihinko <borgah12345@gmail.com>
2025-12-08 20:33:57 +0300

    edit scripts
    need fix

--- New_Bermudes/Assets/Scripts/PlayerHealth.cs
+++ New_Bermudes/Assets/Scripts/PlayerHealth.cs
@@ -2,44 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth = 100;
-    public Bars bar;
-    public int addXP = 3;
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        Debug.Log($"Игрок получил {damage} урона! Осталось HP: {currentHealth}");
+    public Slider slider;
 
-        if (currentHealth <= 0)
+    public void Update()
+    {
+        slider.value = currentHealth;
+        if (currentHealth > 100)
+        {
+            currentHealth = 100;
+        }
+        else
         {
             Die();
         }
-        bar.indexofImage = 0;
-        bar.SetFillAmount(currentHealth);
     }
-
     private void Die(int index = 0)
     {
         SceneManager.LoadScene(index);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("pharmacy"))
-        {
-            if (currentHealth != 100)
-            {
-                currentHealth += addXP;
-                Debug.Log("Хил");
-            }
-        }
-        else if(other.gameObject.CompareTag("monster"))
-        {
-            currentHealth -= 10;
-            Debug.Log("монстры атакуют");
-        }
-    }
 }
--- New_Bermudes/Assets/Scripts/death.cs
+++ New_Bermudes/Assets/Scripts/death.cs
@@ -5,19 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class death : MonoBehaviour
 {
-    public GameObject cam1;
-    public GameObject cam2;
-    public hp hp;
-    void Start()
-    {
-        cam1.gameObject.SetActive(false);
-    }
+    //public GameObject cam1;
+    //public GameObject cam2;
+    //public PlayerHealth health;
+    //void Start()
+    //{
+    //    cam1.gameObject.SetActive(false);
+    //}
 
-    void Update()
-    {
-        if(hp.health <= 0f)
-        {
-            SceneManager.LoadScene(4);
-        }
-    }
+    //void Update()
+    //{
+    //    if( <= 0)
+    //    {
+    //        SceneManager.LoadScene(4);
+    //    }
+    //}
 }
